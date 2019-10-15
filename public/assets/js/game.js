916591b8dce4cc3035acc41a9dc9d9f631bb07ae
HopeEternal <risalee316@gmail.com>
2019-10-15 12:07:52 -0600

    Other players added to ui

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -33,6 +33,7 @@ class WorldScene extends Phaser.Scene {
 
   create() {
     this.socket = io();
+    this.otherPlayers = this.physics.add.group();
    
     // create map
     this.createMap();
@@ -61,36 +62,108 @@ class WorldScene extends Phaser.Scene {
       this.addOtherPlayers(playerInfo);
     }.bind(this));
   }
-  
-  createPlayer() {
-    // our player sprite created through the phycis system
-    this.player = this.physics.add.sprite(50, 100, 'player', 6);
-  
-    // don't go out of the map
-    this.player.setCollideWorldBounds(true);
+
+createMap() {
+  // create the map
+  this.map = this.make.tilemap({
+    key: 'map'
+  });
+
+  // first parameter is the name of the tilemap in tiled
+  var tiles = this.map.addTilesetImage('spritesheet', 'tiles', 16, 16, 1, 2);
+
+  // creating the layers
+  this.map.createStaticLayer('Grass', tiles, 0, 0);
+  this.map.createStaticLayer('Obstacles', tiles, 0, 0);
+
+  // don't go out of the map
+  this.physics.world.bounds.width = this.map.widthInPixels;
+  this.physics.world.bounds.height = this.map.heightInPixels;
+}
+
+createAnimations() {
+  //  animation with key 'left', we don't need left and right as we will use one and flip the sprite
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('player', {
+      frames: [1, 7, 1, 13]
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  // animation with key 'right'
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('player', {
+      frames: [1, 7, 1, 13]
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'up',
+    frames: this.anims.generateFrameNumbers('player', {
+      frames: [2, 8, 2, 14]
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'down',
+    frames: this.anims.generateFrameNumbers('player', {
+      frames: [0, 6, 0, 12]
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+}
+
+createPlayer(playerInfo) {
+  // our player sprite created through the physics system
+  this.player = this.add.sprite(0, 0, 'player', 6);
+ 
+  this.container = this.add.container(playerInfo.x, playerInfo.y);
+  this.container.setSize(16, 16);
+  this.physics.world.enable(this.container);
+  this.container.add(this.player);
+ 
+  // update camera
+  this.updateCamera();
+ 
+  // don't go out of the map
+  this.container.body.setCollideWorldBounds(true);
+}
+
+addOtherPlayers(playerInfo) {
+  const otherPlayer = this.add.sprite(playerInfo.x, playerInfo.y, 'player', 9);
+  otherPlayer.setTint(Math.random() * 0xffffff);
+  otherPlayer.playerId = playerInfo.playerId;
+  this.otherPlayers.add(otherPlayer);
+}
+
+updateCamera() {
+  // limit camera to map
+  this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+  this.cameras.main.startFollow(this.container);
+  this.cameras.main.roundPixels = true; // avoid tile bleed
+}
+
+createEnemies() {
+  // where the enemies will be
+  this.spawns = this.physics.add.group({
+    classType: Phaser.GameObjects.Zone
+  });
+  for (var i = 0; i < 30; i++) {
+    var x = Phaser.Math.RND.between(0, this.physics.world.bounds.width);
+    var y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
+    // parameters are x, y, width, height
+    this.spawns.create(x, y, 20, 20);
   }
-  
-  updateCamera() {
-    // limit camera to map
-    this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
-    this.cameras.main.startFollow(this.player);
-    this.cameras.main.roundPixels = true; // avoid tile bleed
-  }
-  
-  createEnemies() {
-    // where the enemies will be
-    this.spawns = this.physics.add.group({
-      classType: Phaser.GameObjects.Zone
-    });
-    for (var i = 0; i < 30; i++) {
-      var x = Phaser.Math.RND.between(0, this.physics.world.bounds.width);
-      var y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
-      // parameters are x, y, width, height
-      this.spawns.create(x, y, 20, 20);
-    }
-    // add collider
-    this.physics.add.overlap(this.player, this.spawns, this.onMeetEnemy, false, this);
-  }
+
+}
 
   onMeetEnemy(player, zone) {
     // we move the zone to some other location
@@ -99,37 +172,37 @@ class WorldScene extends Phaser.Scene {
   }
 
   update() {
-    // this.controls.update(delta);
-
-    this.player.body.setVelocity(0);
-
-    // Horizontal movement
-    if (this.cursors.left.isDown) {
-      this.player.body.setVelocityX(-80);
-    } else if (this.cursors.right.isDown) {
-      this.player.body.setVelocityX(80);
-    }
-
-    // Vertical movement
-    if (this.cursors.up.isDown) {
-      this.player.body.setVelocityY(-80);
-    } else if (this.cursors.down.isDown) {
-      this.player.body.setVelocityY(80);
-    }
-
-    // Update the animation last and give left/right animations precedence over up/down animations
-    if (this.cursors.left.isDown) {
-      this.player.anims.play('left', true);
-      this.player.flipX = true;
-    } else if (this.cursors.right.isDown) {
-      this.player.anims.play('right', true);
-      this.player.flipX = false;
-    } else if (this.cursors.up.isDown) {
-      this.player.anims.play('up', true);
-    } else if (this.cursors.down.isDown) {
-      this.player.anims.play('down', true);
-    } else {
-      this.player.anims.stop();
+    if (this.container) {
+      this.container.body.setVelocity(0);
+   
+      // Horizontal movement
+      if (this.cursors.left.isDown) {
+        this.container.body.setVelocityX(-80);
+      } else if (this.cursors.right.isDown) {
+        this.container.body.setVelocityX(80);
+      }
+   
+      // Vertical movement
+      if (this.cursors.up.isDown) {
+        this.container.body.setVelocityY(-80);
+      } else if (this.cursors.down.isDown) {
+        this.container.body.setVelocityY(80);
+      }
+   
+      // Update the animation last and give left/right animations precedence over up/down animations
+      if (this.cursors.left.isDown) {
+        this.player.anims.play('left', true);
+        this.player.flipX = true;
+      } else if (this.cursors.right.isDown) {
+        this.player.anims.play('right', true);
+        this.player.flipX = false;
+      } else if (this.cursors.up.isDown) {
+        this.player.anims.play('up', true);
+      } else if (this.cursors.down.isDown) {
+        this.player.anims.play('down', true);
+      } else {
+        this.player.anims.stop();
+      }
     }
   }
 }
